39cb903fc85e7f702289a76bfe88f9a57e9e45a0
BHANU PARTAP <148768482+Bhanu-partap-13@users.noreply.github.com>
2024-07-07 11:45:58 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -3,3 +3,5 @@ music={"my eyes": "https://youtu.be/atmGkTfyyjc?si=aiVL0sAlRQIGqMXg",
       "hind ke sitara":"https://youtu.be/ZF6t-XGdkRQ?si=AVE8Usb9qAtldxUQ",
       "sajni":"https://youtu.be/k3g_WjLCsXM?si=SkYbK1TfItqm_Cwy"
       }
+
+#you can choose music of your wish even you can also add the Playlists you can add as many songs you can 
